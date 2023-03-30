d9a5618d42ef4dfe16ccdc83866f4182c615cc3b
Elher Encarnacion <encarnacionelher@gmail.com>
2023-03-30 20:55:10 +0800

    added container and gallery

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+// Loading area function
+
+document.onreadystatechange = function() {
+  if (document.readyState !== "complete") {
+      document.querySelector(
+        "body").style.visibility = "hidden";
+      document.querySelector(
+        "#loading").style.visibility = "visible";
+  } else {
+      document.querySelector(
+        "#loading").style.display = "none";
+      document.querySelector(
+        "body").style.visibility = "visible";
+  }
+};
+
+
 document.addEventListener('slide.bs.carousel', function(event){
     var activeSlide = event.to;
 switch (activeSlide) {
